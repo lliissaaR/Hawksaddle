c74f7ccbc35bf38b8e808998b899f0dfae31daf3
Lisa Roche <lisa@Lisas-MacAir.local>
2023-03-29 17:14:24 +0200

    pas mal de modifs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'  // npm install express
 import ejs from 'ejs' // npm install ejs
 import bodyParser from 'body-parser'
 import mysql from 'mysql2'
+import session from 'express-session'
 
 
 
@@ -16,20 +17,28 @@ const connexion = mysql.createConnection({  //on cré une connexion à la base d
 
 })
 
-connexion.query("SELECT * FROM produits", )
+
+function ProduitDansPanier(panier, id) {
+    for (let index = 0; index < panier.length; index++) {
+        if (panier[index].id === id) {
+            return true
+        }
+    }
+
+    return false
+}
 
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(express.static('public')) // l'application utilise le dossier public par defaut sans avoir a specifier le chemin
 
+app.use(session({secret:"secret"}))
+
 app.set('view engine', 'ejs') // va dire a expres de set le view engine avec ejs
 
 
 app.get('/', function(httpRequest, httpResponse) {  //création d'une route avec la methode http Get donc le chemin est '/'
-    
-    console.log('objet request: ',httpRequest)  //log de la requete entrante dans la console
-   
-    
+       
     httpResponse.render('pages/index') //envoie de la reponse http, .render fonctionne que si on a fait le app.set('view engine', 'ejs'), render va aller chercher automatiquement dans le dossier views
 })
 
@@ -38,14 +47,12 @@ app.listen('3030') // aller sur http://127.0.0.1:3030 et le message se log
 
 
 
-app.get('/Accessoires_equestres', function(httpRequest, httpResponse) {
-   
-    httpResponse.render('pages/Accessoires_equestres')
-})
-
 app.get('/Brides', function(httpRequest, httpResponse) {
    
-    httpResponse.render('pages/Brides')
+    connexion.query("SELECT * FROM produits WHERE categorie='Brides'",(err, result) => {
+        console.log(result)
+        httpResponse.render('pages/Brides', {produits:result})
+})
 })
 
 app.get('/Contact', function(httpRequest, httpResponse) {
@@ -55,12 +62,18 @@ app.get('/Contact', function(httpRequest, httpResponse) {
 
 app.get('/Fauconnerie', function(httpRequest, httpResponse) {
    
-    httpResponse.render('pages/Fauconnerie')
+    connexion.query("SELECT * FROM produits WHERE categorie = 'Fauconnerie'",(err, result) => {
+        
+        httpResponse.render('pages/Fauconnerie', {produits:result})
+})
 })
 
 app.get('/Selles', function(httpRequest, httpResponse) {
    
-    httpResponse.render('pages/Selles')
+    connexion.query("SELECT * FROM produits WHERE categorie = 'Selles'",(err, result) => {
+        
+        httpResponse.render('pages/Selles', {produits:result})
+    })
 })
 
 app.get('/Panier', function(httpRequest, httpResponse) {
@@ -70,7 +83,10 @@ app.get('/Panier', function(httpRequest, httpResponse) {
 
 app.get('/Accessoires_equestres', function(httpRequest, httpResponse) {
    
-    httpResponse.render('pages/Accessoires_equestres')
+    connexion.query("SELECT * FROM `produits` WHERE `categorie` = 'Accessoires_equestres'", (err, result) => {
+        
+        httpResponse.render('pages/Accessoires_equestres', {produits:result})
+})
 })
 
 app.get('/collier_chasse1', function(httpRequest, httpResponse) {
@@ -125,7 +141,10 @@ app.get('/selle_show', function(httpRequest, httpResponse) {
 
 app.get('/chaperon', function(httpRequest, httpResponse) {
    
-    httpResponse.render('pages/pages_produit/détail fauconnerie/chaperon')
+    connexion.query("SELECT * FROM produits",(err, result) => {
+        
+        httpResponse.render('pages/pages_produit/détail fauconnerie/chaperon', {produits:result})
+    })
 })
 
 app.get('/gant_aigle', function(httpRequest, httpResponse) {
@@ -182,3 +201,31 @@ app.get('/sidepull', function(httpRequest, httpResponse) {
    
     httpResponse.render('pages/pages_produit/détail brides/sidepull')
 })
+
+
+app.post('/ajouter_au_panier', function(httpRequest, httpResponse) {
+    console.log('httpRequest after ajouter au panier: ', httpRequest.body)
+    let id = httpRequest.body.id
+    let name = httpRequest.body.name
+    let price = httpRequest.body.price
+    let image = httpRequest.body.image
+    let product = {id:id, name:name, price:price, image:image}
+
+    if (httpRequest.session.anier){
+        const panier = httpRequest.session.panier
+        
+        if (!ProduitDansPanier(cart, id)) {
+            panier.push(produit)
+        } 
+    
+    else {
+        httpRequest.session.panier = [produit]
+        const panier = httpRequest.session.panier
+    }
+    }
+
+    httpResponse.render('pages/Panier', {Panier:fakePanier, total:total})
+
+})
+
+
