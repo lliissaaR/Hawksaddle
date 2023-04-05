7de56fb9b444eadb1c08f6790b2d1128f220fce8
Lisa Roche <lisa@Lisas-MacAir.local>
2023-04-05 17:09:55 +0200

    ajouts fonctions

--- index.js
+++ index.js
@@ -44,10 +44,8 @@ app.listen('3030') // dit a l'app d'écouter sur le port 3030 http://127.0.0.1:3
 //redirection des données des tables mysql sur les pages en question
 
 app.get('/Brides', function(httpRequest, httpResponse) {
-   
     connexion.query("SELECT * FROM produits WHERE categorie='Brides'",(err, result) => {
-        console.log(result)
-        httpResponse.render('pages/Brides', {produits:result})
+    httpResponse.render('pages/Brides', {produits:result})
 })
 })
 
@@ -57,18 +55,14 @@ app.get('/Contact', function(httpRequest, httpResponse) {
 })
 
 app.get('/Fauconnerie', function(httpRequest, httpResponse) {
-   
-    connexion.query("SELECT * FROM produits WHERE categorie = 'Fauconnerie'",(err, result) => {
-        
-        httpResponse.render('pages/Fauconnerie', {produits:result})
+    connexion.query("SELECT * FROM produits WHERE categorie = 'Fauconnerie'",(err, result) => {   
+    httpResponse.render('pages/Fauconnerie', {produits:result})
 })
 })
 
 app.get('/Selles', function(httpRequest, httpResponse) {
-   
     connexion.query("SELECT * FROM produits WHERE categorie = 'Selles'",(err, result) => {
-        
-        httpResponse.render('pages/Selles', {produits:result})
+    httpResponse.render('pages/Selles', {produits:result})
     })
 })
 
@@ -81,130 +75,142 @@ app.get('/Panier', function(httpRequest, httpResponse) {
     
 })
 
-app.get('/Accessoires_equestres', function(httpRequest, httpResponse) {
-   
+app.get('/Accessoires_equestres', function(httpRequest, httpResponse) { 
     connexion.query("SELECT * FROM `produits` WHERE `categorie` = 'Accessoires_equestres'", (err, result) => {
-        
-        httpResponse.render('pages/Accessoires_equestres', {produits:result})
+    httpResponse.render('pages/Accessoires_equestres', {produits:result})
 })
 })
 
 app.get('/collier_chasse1', function(httpRequest, httpResponse) {
-    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Accessoires_equestres'", (err, result) => {
+    connexion.query("SELECT * FROM `produits` WHERE `id` = '11'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail_accessoires/collier_chasse1', {produits:result})
 })
+})
+
 
 app.get('/collier_chasse2', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `id` = '12'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail_accessoires/collier_chasse2', {produits:result})
+})
 })
 
 app.get('/lasso', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name`= 'Lasso'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail_accessoires/lasso', {produits:result})
+})
 })
 
 app.get('/tapis_rando', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Tapis de Rando'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail_accessoires/tapis_rando', {produits:result})
 })
-
+})
 app.get('/tapis', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Tapis'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail_accessoires/tapis', {produits:result})
 })
-
+})
 app.get('/selle_barrel', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Selle de Barrel'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail selles/selle_barrel', {produits:result})
+})
 })
 
 app.get('/selle_cutting', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Selle de Cutting'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail selles/selle_cutting', {produits:result})
+})
 })
 
 app.get('/selle_pleasure', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Selle de Pleasure'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail selles/selle_pleasure', {produits:result})
+})
 })
 
 app.get('/selle_reining', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Selle de Reining'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail selles/selle_reining', {produits:result})
+})
 })
 
 app.get('/selle_show', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Selle de Show'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail selles/selle_show', {produits:result})
+})
 })
 
 app.get('/chaperon', function(httpRequest, httpResponse) {
-   
     connexion.query("SELECT * FROM produits WHERE name = 'Chaperon'",(err, result) => {
         httpResponse.render('pages/pages_produit/détail fauconnerie/chaperon', {produits:result})
         
-    })
-    
+    })    
 })
 
 app.get('/gant_aigle', function(httpRequest, httpResponse) {
-
     connexion.query("SELECT * FROM produits WHERE name = 'Gant Taille Aigle'", (err, result) => {
         httpResponse.render('pages/pages_produit/détail fauconnerie/gant_aigle', {produits:result})
     })
-   
-    
 })
 
 app.get('/gant_buse', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Gant Taille Buse'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail fauconnerie/gant_buse', {produits:result})
+})
 })
 
 app.get('/gant_faucon', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Gant Taille Faucon'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail fauconnerie/gant_faucon', {produits:result})
+})
 })
 
 app.get('/jets', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Jets'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail fauconnerie/jets', {produits:result})
+})
 })
 
 app.get('/leurre', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Leurre'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail fauconnerie/leurre', {produits:result})
+})
 })
 
 app.get('/sacoches', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Sacoches'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail fauconnerie/sacoches', {produits:result})
+})
 })
 
 app.get('/bride_simple', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Bride Simple'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail brides/bride_simple', {produits:result})
+})
 })
 
 app.get('/bride_sous_gorge', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Bride avec Sous Gorge'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail brides/bride_sous_gorge', {produits:result})
+})
 })
 
 app.get('/bridon_show', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Bridon de Show'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail brides/bridon_show', {produits:result})
+})
 })
 
 app.get('/hackamors', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Hackamors'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail brides/hackamors', {produits:result})
+})
 })
 
 app.get('/sidepull', function(httpRequest, httpResponse) {
-   
+    connexion.query("SELECT * FROM `produits` WHERE `name` = 'Sidepull'", (err, result) => {
     httpResponse.render('pages/pages_produit/détail brides/sidepull', {produits:result})
+})
 })
 
 // ajout produit au panier
